67441ab683d8d9eecc3bbc0c36d1a79db4fac633
Damian <jimmii@o2.pl>
2018-01-24 22:39:21 +0100

    Add project files.

---
+++ kalkulator/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kalkulator
+{
+    public partial class Form1 : Form
+    {
+        double variable;
+        char sign;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("0");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Text="";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("1");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("2");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("3");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("4");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("5");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("6");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("7");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("8");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("9");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(".");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            int textLength = textBox1.Text.Length;
+
+            if (textLength > 0)
+                textBox1.Text = textBox1.Text.Substring(0, textLength - 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int textLength = textBox1.Text.Length;
+                if (textLength == 0)
+                    {
+                        textBox1.Text = "-" + textBox1.Text;
+                    }
+                else if ((textBox1.Text[0]).Equals('-'))
+                    textBox1.Text = textBox1.Text.Substring(1, textLength - 1);           
+                else 
+                    {
+                    textBox1.Text = "-" + textBox1.Text;
+                    }
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                variable = double.Parse(textBox1.Text);
+                sign = '+';
+                textBox1.Text = "";
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                variable = double.Parse(textBox1.Text);
+                sign = '-';
+                textBox1.Text = "";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                variable = double.Parse(textBox1.Text);
+                sign = '*';
+                textBox1.Text = "";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                variable = double.Parse(textBox1.Text);
+                sign = '/';
+                textBox1.Text = "";
+            }
+        }
+        private void button19_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text!="")
+            textBox1.Text = (Math.Sqrt(double.Parse(textBox1.Text))).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            switch(sign)
+            {
+                case '+':
+                    textBox1.Text = (variable + double.Parse(textBox1.Text)).ToString();
+                    break;
+
+                case '-':
+                    textBox1.Text = (variable - double.Parse(textBox1.Text)).ToString();
+                    break;
+
+                case '*':
+                    textBox1.Text = (variable * double.Parse(textBox1.Text)).ToString();
+                    break;
+
+                case '/':
+                    if(textBox1.TextLength==1 && textBox1.Text[0].Equals('0'))
+                    {
+                        textBox1.Text = "Nie dzielimy przez zero!";
+                    }
+                    else
+                    {
+                        textBox1.Text = ((double)variable / double.Parse(textBox1.Text)).ToString();
+                    }
+                    
+                    break;
+            }
+        }
+
+        
+    }
+}
